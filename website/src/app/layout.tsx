f41bb86892e4747ba5a068fc7465ff405d0e700a
0xviny <vgalvaomarcelino@gmail.com>
2026-01-08 22:19:08 -0300

    feat: add @vercel/analytics for enhanced tracking in layout component

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -2,6 +2,7 @@ import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -78,7 +79,10 @@ export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider>
+          {children}
+          <Analytics />
+        </RootProvider>
       </body>
     </html>
   );
